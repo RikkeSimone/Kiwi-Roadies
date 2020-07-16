1c738d59726b7cd5123d710bdcf9fcbe8bbbc658
ariana-mirus <arianamirus@gmail.com>
2020-07-16 22:31:31 +1200

    created route to bring in DOC API currently not working, trying to troubleshoot in postman

--- server/routes/coolstuff.js
+++ server/routes/coolstuff.js
@@ -1,4 +1,24 @@
 const express = require('express')
+const { request } = require('../server')
 const router = express.Router()
 
+// /api/v1/coolstuff
+router.get('/campsites', (req, res) => {
+  console.log(request)
+  request.get('https://api.doc.govt.nz/v2/campsites', 'accept: application/json', 'x-api-key: eOPSTyEUYA1cnX8as3KLY5Dtmx8wbxqg8dQLNweL')
+    .then(response => {
+      res.status(200).json(response.body)
+    })
+    .catch(err => {
+      res.status(500).send('API error: ' + err.message)
+    })
+})
+
 module.exports = router
+
+// api.doc.govt.nz/v2
+// gets you v2 campsites
+
+// request.get('https://api.doc.govt.nz/v2' + '?q=' + 'eOPSTyEUYA1cnX8as3KLY5Dtmx8wbxqg8dQLNweL')
+
+// "https://api.doc.govt.nz/v2/campsites" -H "accept: application/json" -H "x-api-key: eOPSTyEUYA1cnX8as3KLY5Dtmx8wbxqg8dQLNweL"
