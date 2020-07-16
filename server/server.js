2494b5dfebe8594e6398fed672b2166b6192d050
ariana-mirus <arianamirus@gmail.com>
2020-07-16 13:51:04 +1200

    updated server.js on backend

--- server/server.js
+++ server/server.js
@@ -3,7 +3,20 @@ const express = require('express')
 
 const server = express()
 
+// reference code - to be deleted at EOD
+// const foods = require('./routes/foods')
+// const categories = require('./routes/categories')
+
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
+server.use('/api/v1/')
+
+// reference code to be deleted by EOD
+// server.use('/api/v1/categories', categories)
+
+
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+})
 
 module.exports = server
