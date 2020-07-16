ca5b7da1c63e52bf01af0a1b83f65d9423cf996d
ariana-mirus <arianamirus@gmail.com>
2020-07-17 11:43:56 +1200

    added env file for token, api call can work on server side

--- server/routes/coolstuff.js
+++ server/routes/coolstuff.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const request = require('superagent')
+require('dotenv').config()
 const router = express.Router()
-
-const dockey = 'eOPSTyEUYA1cnX8as3KLY5Dtmx8wbxqg8dQLNweL'
 
 // /api/v1/coolstuff
 router.get('/campsites', (req, res) => {
   request.get('https://api.doc.govt.nz/v2/campsites')
-    .set('X-API-Key', 'eOPSTyEUYA1cnX8as3KLY5Dtmx8wbxqg8dQLNweL')
+    .set('X-API-Key', `${process.env.DOC_TOKEN}`)
     .set('accept', 'application/json')
     .then(response => {
       res.status(200).json(response.body)
