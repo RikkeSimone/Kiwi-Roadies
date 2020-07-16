ad2af0b7c58af7660392b4af2c5d7539d146eec8
Kim Fitter <26913197+kimnewzealand@users.noreply.github.com>
2020-07-17 11:18:04 +1200

    Updated linting to check push action

--- client/components/MainMap.jsx
+++ client/components/MainMap.jsx
@@ -3,10 +3,9 @@ import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 import { Icon } from 'leaflet'
 
 const position = [-36.8, 174.7]
-export default function App() {
+export default function App () {
   return (
-   
-    <Map  center={position} zoom={8}>
+    <Map center={position} zoom={8}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
