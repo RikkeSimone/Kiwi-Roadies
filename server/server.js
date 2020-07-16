6cc6bff4dc261fd6b71f67064fc974ad064a00b9
Rikke Simone <33711718+RikkeSimone@users.noreply.github.com>
2020-07-16 15:32:26 +1200

    updated the 404 route for consistency

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ server.use(express.static(path.join(__dirname, 'public')))
 server.use('/api/v1/coolstuff', coolstuff)
 server.use('/api/v1/form', form)
 server.use('/api/v1/search', search)
-server.use('/v1/*', (req, res) => res.sendStatus(404))
+server.use('/api/v1/*', (req, res) => res.sendStatus(404))
 
 server.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'))
