f833dfeeb5c2e072d1e23d0aef5c6f298a93fa80
Jonty Buchan <63333241+jonty-terrence@users.noreply.github.com>
2020-07-16 14:53:23 +1200

    Reconfigured webpack and package json

--- client/webpack.common.config.js
+++ client/webpack.common.config.js
@@ -3,21 +3,19 @@ const path = require('path')
 module.exports = {
   entry: './client/index.js',
   output: {
-    path: path.join(__dirname, 'server/public'),
+    path: path.join(__dirname, '../server/public'),
     filename: 'bundle.js'
   },
-  mode: 'development',
   module: {
     rules: [
       {
-        loader: 'babel-loader',
         test: /\.jsx?$/,
+        loader: 'babel-loader',
         exclude: /node_modules/
       }
     ]
   },
   resolve: {
     extensions: ['.js', '.jsx']
-  },
-  devtool: 'source-map'
+  }
 }
