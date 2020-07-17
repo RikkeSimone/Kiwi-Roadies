246eb5a6e03684c2a5797ce6a4077b34b1edc032
Kim Fitter <26913197+kimnewzealand@users.noreply.github.com>
2020-07-17 15:45:59 +1200

    Centered map in div and chnaged MainMap to redux to get state trip data

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,13 +3,14 @@ import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
 import Hello from './Hello'
-// import MainMap from './MainMap'
+import MainMap from './MainMap'
 
 const App = () => (
   <div className='app-container'>
     <div className="body">
       <Header />
       <Hello />
+      <MainMap />
     </div>
     <Footer />
   </div>
--- client/components/MainMap.jsx
+++ client/components/MainMap.jsx
@@ -1,14 +1,37 @@
 import React from 'react'
+import { Icon, Marker, Popup } from 'leaflet'
 import { Map, TileLayer } from 'react-leaflet'
+import { connect } from 'react-redux'
 
-const position = [-36.8, 174.7]
-export default function App () {
-  return (
-    <Map center={position} zoom={8}>
+// coordinates to center map
+const position = [-40.852931, 172.762057]
+
+const car = new Icon({
+  iconUrl: './graphics/kiwiroadieslogo.png',
+  iconSize: [25, 25]
+})
+
+const MainMap = ({ dataSet }) => (
+  <div className='map-container'>
+    <Map center={position} zoom={5} >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
+      {/* <Marker position={position}>
+        <Popup>
+            CAR
+        </Popup> */}
+      {/* </Marker> */}
     </Map>
-  )
+  </div>
+)
+
+const mapStateToProps = (state) => {
+  console.log('state:', state.dataSet.trips[0])
+  return {
+    dataSet: state.dataSet
+  }
 }
+
+export default connect(mapStateToProps)(MainMap)
