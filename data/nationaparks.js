2db3bf4ab6b375c238a7189662ec577c94cce4e3
ariana-mirus <arianamirus@gmail.com>
2020-07-19 09:48:09 +1200

    created new national parks data file shell

---
+++ data/nationaparks.js
@@ -0,0 +1,122 @@
+export default {
+  nationalParks: [
+    {
+      id: 1,
+      name: '',
+      areaKm: '',
+      lat: '',
+      long: '',
+      numHuts: '',
+      region: ''
+    },
+    {
+      id: 2,
+      name: '',
+      areaKm: '',
+      lat: '',
+      long: '',
+      numHuts: '',
+      region: ''
+    },
+    {
+      id: 3,
+      name: '',
+      areaKm: '',
+      lat: '',
+      long: '',
+      numHuts: '',
+      region: ''
+    },
+    {
+      id: 4,
+      name: '',
+      areaKm: '',
+      lat: '',
+      long: '',
+      numHuts: '',
+      region: ''
+    },
+    {
+      id: 5,
+      name: '',
+      areaKm: '',
+      lat: '',
+      long: '',
+      numHuts: '',
+      region: ''
+    },
+    {
+      id: 6,
+      name: '',
+      areaKm: '',
+      lat: '',
+      long: '',
+      numHuts: '',
+      region: ''
+    },
+    {
+      id: 7,
+      name: '',
+      areaKm: '',
+      lat: '',
+      long: '',
+      numHuts: '',
+      region: ''
+    },
+    {
+      id: 8,
+      name: '',
+      areaKm: '',
+      lat: '',
+      long: '',
+      numHuts: '',
+      region: ''
+    },
+    {
+      id: 9,
+      name: '',
+      areaKm: '',
+      lat: '',
+      long: '',
+      numHuts: '',
+      region: ''
+    },
+    {
+      id: 10,
+      name: '',
+      areaKm: '',
+      lat: '',
+      long: '',
+      numHuts: '',
+      region: ''
+    },
+    {
+      id: 11,
+      name: '',
+      areaKm: '',
+      lat: '',
+      long: '',
+      numHuts: '',
+      region: ''
+    },
+    {
+      id: 12,
+      name: '',
+      areaKm: '',
+      lat: '',
+      long: '',
+      numHuts: '',
+      region: ''
+    },
+    {
+      id: 13,
+      name: '',
+      areaKm: '',
+      lat: '',
+      long: '',
+      numHuts: '',
+      region: ''
+    }
+
+  ]
+}
