8d11be471bdf347f60f4a5d07eac0fd2dbcfa0b2
Kim Fitter <kimfitter@yahoo.com>
2020-07-19 20:02:18 +1200

    Added Marker and popup to map

--- client/components/MainMap.jsx
+++ client/components/MainMap.jsx
@@ -2,20 +2,25 @@ import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import 'regenerator-runtime/runtime'
 
-import MapGL from 'react-map-gl'
+import ReactMapGL, { Marker, Popup, Layer, ControlPanel, NavigationControl } from 'react-map-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
+// import DeckGL, { ArcLayer } from 'deck.gl'
 
 // coordinates to center map
-const position = [172.762057, -40.852931]
+// const position = [172.762057, -40.852931]
 
-const from = [175, -36]
-const to = [175.5413, -38.13]
+// some inspiration https://greatescape.co/
 
 console.log('mapbox token', process.env.ACCESS_TOKEN)
 
 const MAPBOX_TOKEN = 'pk.eyJ1Ijoia2ltbmV3emVhbGFuZCIsImEiOiJja2NzY3Q3anMxaXY4MnltazVsdHMxNGMyIn0.Vpc0ObF_wq6PfnZTfWTxJQ'
-// Leaflet controls examples, leaflet maps maintain their own state. todo add ref to Map
-// https://www.azavea.com/blog/2016/12/05/getting-started-with-react-and-leaflet/
+
+const navStyle = {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  padding: '10px'
+}
 
 const MainMap = (props) => {
   const { dataSet } = props
@@ -29,17 +34,46 @@ const MainMap = (props) => {
     pitch: 0
   }
 
+  const source = {
+    latitude: -36,
+    longitude: 175
+  }
+
+  const target = {
+    latitude: -38.13,
+    longitude: 175.5413
+  }
+
   return (
-    <div>
+    <div className="root">
       <p>map</p>
-      <MapGL
+      <ReactMapGL
         {...viewport}
         width="60vw"
         height="80vh"
         mapStyle="mapbox://styles/mapbox/light-v10"
         onViewportChange={viewport => viewport}
         mapboxApiAccessToken={MAPBOX_TOKEN}
-      />
+      >
+        <Marker
+          longitude={source.longitude}
+          latitude={source.latitude}
+          offsetTop={-20}
+          offsetLeft={-10}
+        >
+Marker
+        </Marker>
+        <Popup
+          longitude={source.longitude}
+          latitude={source.latitude}
+          // onClose={closePopup}
+          closeButton={true}
+          closeOnClick={true}
+          // offsetTop={-30}
+        >
+          <p>Marker</p>
+        </Popup>
+      </ReactMapGL>
     </div>
   )
 }
