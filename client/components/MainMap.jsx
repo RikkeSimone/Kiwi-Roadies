2071f1230b71abcac16b32fa68b508800d7183ca
Kim Fitter <26913197+kimnewzealand@users.noreply.github.com>
2020-07-19 13:03:01 +1200

    Add mapbox token to .env, to use with new map

--- client/components/MainMap.jsx
+++ client/components/MainMap.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import { Icon } from 'leaflet'
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import { Map, TileLayer, Marker } from 'react-leaflet'
 
 import { connect } from 'react-redux'
-// import Routing from 'react-native-leaflet-routing'
 import 'regenerator-runtime/runtime'
 const stamenTonerTiles = 'http://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}.png'
 const stamenTonerAttr = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
@@ -15,7 +14,7 @@ const position = [-40.852931, 172.762057]
 
 const carIcon = new Icon({
   iconUrl: './graphics/just-kiwi.png',
-  iconSize: [30, 30],
+  iconSize: [30, 30]
   // iconAnchor: [15, 42]
 })
 
@@ -28,12 +27,8 @@ const endIcon = new Icon({
 
 const from = [-36, 175]
 const to = [-38.13, 175.5413]
-const ownPositionMarker = null
 
-// Routing examples
-// https://github.com/arjunghimire/react-leaflet-routing-machine-example
-
-// https://github.com/sgj0/react-native-leaflet-routing
+console.log('mapbox token', process.env.accessToken)
 
 // Leaflet controls examples, leaflet maps maintain their own state. todo add ref to Map
 // https://www.azavea.com/blog/2016/12/05/getting-started-with-react-and-leaflet/
@@ -42,19 +37,6 @@ const MainMap = (props) => {
   const { dataSet } = props
   console.log('the dataSet is for the trip planning is ', dataSet)
 
-  const componentDidMount = async () => {
-    let initialRegion = { from }
-    try {
-      console.log('current location', initialRegion)
-    } catch (e) {
-      console.log(e)
-    } finally {
-      ownPositionMarker = {
-        id: 'position',
-        coords: initialRegion
-      }
-    }
-  }
   return (
     <div className='map-container'>
       <Map
@@ -66,19 +48,6 @@ const MainMap = (props) => {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
-        {/* <Routing
-        // optional: map layer
-        mapLayer= {mapLayers}
-
-        // optional : initial region displayed
-        initialRegion={from}
-
-        // optional: coordinates of the starting point
-        from={from}
-
-        // optional: coordinates of the arriving point
-        to={endPosition}
-      /> */}
         <TileLayer
           attribution={stamenTonerAttr}
           url={stamenTonerTiles}
