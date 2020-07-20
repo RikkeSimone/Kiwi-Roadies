cba06fcfd0647d9e96e93f45eb2e73875e0541de
Jonty Buchan <63333241+jonty-terrence@users.noreply.github.com>
2020-07-20 15:31:45 +1200

    Animate loading component

--- client/components/Loading.jsx
+++ client/components/Loading.jsx
@@ -4,9 +4,9 @@ import { connect } from 'react-redux'
 class Loading extends React.Component {
   render () {
     return (
-      <>
-        <div className="loader"></div>
-      </>
+      <div className="loading-screen-image">
+        <img src="./graphics/loadingscreen-revisioned.png" className="kiwi-car"/>
+      </div>
     )
   }
 }
