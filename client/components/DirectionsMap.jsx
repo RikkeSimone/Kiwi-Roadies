42d8d8ad34f9c5cd204544eb7ddad454d315fa87
Kim Fitter <kimfitter@yahoo.com>
2020-07-20 21:43:21 +1200

    Add directions and geocoder to DirectionsMap test component

--- client/components/DirectionsMap.jsx
+++ client/components/DirectionsMap.jsx
@@ -4,6 +4,9 @@ import 'regenerator-runtime/runtime'
 
 import mapboxgl from 'mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
+import MapBoxDirections from 'mapbox-gl-directions'
+import 'mapbox-gl/dist/mapbox-gl-directions.css'
+import MapboxGeocoder from 'mapbox-gl-geocoder'
 
 require('dotenv').config()
 
@@ -23,6 +26,21 @@ class DirectionsMap extends Component {
       center: [172.76205, -40.852931],
       zoom: 4
     })
+
+    const directions = new MapBoxDirections({
+      accessToken: mapboxgl.accessToken,
+      unit: 'metric',
+      profile: 'mapbox/driving'
+    })
+
+    map.addControl(directions, 'bottom-left')
+
+    const geocoder = new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      types: 'country,region,place,postcode,locality,neighborhood'
+    })
+
+    geocoder.addTo('#geocoder')
   }
 
   render () {
