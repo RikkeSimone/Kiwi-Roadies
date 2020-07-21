2859ecb49d722dce4256f0cf35dad8d9764a2408
ariana-mirus <arianamirus@gmail.com>
2020-07-21 12:21:24 +1200

    updated trip data to individual locations with lat and long

--- data/trips.js
+++ data/trips.js
@@ -2,34 +2,33 @@ export default {
   trips: [
     {
       id: 1,
-      start: 'Auckland',
-      startlat: -36.8485,
-      startlong: 174.7633,
-      end: 'Thames',
-      endlat: -37.13832,
-      endlong: 175.54011,
+      location: 'Auckland',
+      lat: -36.8485,
+      long: 174.7633,
+      activity: 'Walk',
+      drivetime: 0
+    },
+    {
+      id: 2,
+      location: 'Thames',
+      lat: -37.13832,
+      long: 175.54011,
       activity: 'Walk',
       drivetime: 2
     },
     {
-      id: 2,
-      start: 'Auckland',
-      startlat: -36.8485,
-      startlong: 174.7633,
-      end: 'Raglan',
-      endlat: -37.8014,
-      endlong: 174.8718,
+      id: 3,
+      location: 'Raglan',
+      lat: -37.8014,
+      long: 174.8718,
       activity: 'Beach',
       drivetime: 2.5
     },
     {
-      id: 3,
-      start: 'Auckland',
-      startlat: -36.8485,
-      startlong: 174.7633,
-      end: 'Whangarei',
-      endlat: -35.7251,
-      endlong: 174.3237,
+      id: 4,
+      location: 'Whangarei',
+      lat: -35.7251,
+      long: 174.3237,
       activity: 'Bike',
       drivetime: 2.5
     }
