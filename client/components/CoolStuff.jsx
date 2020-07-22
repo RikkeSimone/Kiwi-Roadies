4e7b453a49ceffc036704333cdb74f692a7cc954
Kim Fitter <26913197+kimnewzealand@users.noreply.github.com>
2020-07-22 14:49:34 +1200

    Merge e2e to latest code

--- client/components/CoolStuff.jsx
+++ client/components/CoolStuff.jsx
@@ -1,20 +1,20 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { addCampsites } from '../actions/campsites'
-import proj4 from 'proj4'
-
-const campURL = 'http://localhost:3000/api/v1/coolstuff/campsites'
-
-// EPSG 2193 NZGD2000 is the proj4string of the doc api data//
-const tmerc = '+proj=tmerc +lat_0=0 +lon_0=173 +k=0.9996 +x_0=1600000 +y_0=10000000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs'
-// input coords World Geodetic System 1984 (G1762) used in conversiom, check this string below?
-const wgs84 = '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs'
+// import proj4 from 'proj4'
 import { addHuts } from '../actions/huts'
 // import { addTracks } from '../actions/tracks'
 import { addTracks } from '../actions/tracks'
 import { getCampsites } from './getCampsites'
 import { getHuts } from './getHuts'
 import { getTracks } from './getTracks'
+
+// const campURL = 'http://localhost:3000/api/v1/coolstuff/campsites'
+
+// // EPSG 2193 NZGD2000 is the proj4string of the doc api data//
+// const tmerc = '+proj=tmerc +lat_0=0 +lon_0=173 +k=0.9996 +x_0=1600000 +y_0=10000000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs'
+// // input coords World Geodetic System 1984 (G1762) used in conversiom, check this string below?
+// const wgs84 = '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs'
 
 class CoolStuff extends Component {
   state= {
